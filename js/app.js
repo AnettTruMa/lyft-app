0a21566b1990f54ea044c5a80c80a95439efbf8c
AnettTruMa <anettp.trujillo@gmail.com>
2018-02-06 03:51:38 -0600

    cambios

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Función de splash
+
+$(document).ready(function(){
+    setTimeout(function() {
+        $(".splash").fadeOut(1500);
+    },1500);
+/* splash fadeIn */
+    setTimeout(function() {
+          $(".content").fadeIn(2500);
+    }, 2500);
+
+});
